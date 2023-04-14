83ed0534a3b59fec90c9b59ad36842df5db48f29
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-04-14 23:06:18 +0600

    chore: reduce dll size and include dll by default

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -8,7 +8,7 @@ New-Item -ItemType Directory -Path $BUILD_DIR
 
 Set-Location $BUILD_DIR
 
-cargo install cargo-xwin
+# cargo install cargo-xwin
 
 function win_build {
     $TARGET = $args[0]
@@ -16,18 +16,20 @@ function win_build {
     $LIBNAME = $args[2]
     
     rustup target add $TARGET
-    cargo xwin build --lib --target $TARGET -r
+    # cargo xwin build --lib --target $TARGET -r
+    cargo build --lib --target $TARGET -r
     New-Item -ItemType Directory -Path $PLATFORM_NAME
-    Copy-Item "../target/$TARGET/release/$LIBNAME" $PLATFORM_NAME/
+    # Copy-Item "../target/$TARGET/release/$LIBNAME" $PLATFORM_NAME/
+    Copy-Item "../target/$TARGET/release/$LIBNAME" .
 }
 
 $WINDOWS_LIBNAME = "smtc_windows.dll"
 
-win_build "aarch64-pc-windows-msvc" "windows-arm64" $WINDOWS_LIBNAME
+# win_build "aarch64-pc-windows-msvc" "windows-arm64" $WINDOWS_LIBNAME
 
 win_build "x86_64-pc-windows-msvc" "windows-x64" $WINDOWS_LIBNAME
 
 
-tar -czvf other.tar.gz windows-*
+# tar -czvf other.tar.gz windows-*
 
-Remove-Item -Recurse -Force windows-*
+# Remove-Item -Recurse -Force windows-*
--- scripts/copylib.ps1
+++ scripts/copylib.ps1
@@ -1,3 +1,5 @@
-$CURR_VERSION = "smtc_windows-v$(Select-String -Path packages/smtc_windows/pubspec.yaml -Pattern '^version: ' | Select-Object -ExpandProperty LineNumber | ForEach-Object { (Get-Content packages/smtc_windows/pubspec.yaml)[$_ - 1] -replace 'version: ', '' })"
+# $CURR_VERSION = "smtc_windows-v$(Select-String -Path packages/smtc_windows/pubspec.yaml -Pattern '^version: ' | Select-Object -ExpandProperty LineNumber | ForEach-Object { (Get-Content packages/smtc_windows/pubspec.yaml)[$_ - 1] -replace 'version: ', '' })"
 
-Copy-Item platform-build/other.tar.gz packages/smtc_windows/windows/$CURR_VERSION.tar.gz
+# Copy-Item platform-build/other.tar.gz packages/smtc_windows/windows/$CURR_VERSION.tar.gz
+
+Copy-Item platform-build/smtc_windows.dll packages/smtc_windows/windows/smtc_windows.dll
